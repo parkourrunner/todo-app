92a790b8ace54a5ab1ed0397ce84e3b7c0ba05d7
Ali Salehi <parkour.runner@gmail.com>
2022-11-11 15:09:42 +0300

    Server done Client init

---
+++ api/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb://localhost:27017/mern-todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post("/todo/new", async (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  todo.save();
+  res.json(todo);
+});
+
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+  res.json(result);
+});
+
+app.put("/todo/edit/:id", async (req, res) => {
+  const todo = await Todo.findByIdAndUpdate(req.params.id, {
+    text: req.body.text,
+  });
+  res.json({ ...todo._doc, text: req.body.text });
+});
+
+app.put("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.complete = !todo.complete;
+  todo.save();
+  res.json({ ...todo._doc });
+});
+
+app.listen(3001, () => {
+  console.log("Listening to 3001");
+});
